f554c007d7c396e45cc83f38b090c63662abc8e7
J-Jenitha <jenithamaxi17@gmail.com>
2025-10-30 23:04:33 +0530

    Delete hotel-admin directory

--- hotel-admin/src/pages/NewRoom.jsx
+++
@@ -1,8 +0,0 @@
-export default function NewRoom(){
-    return (
-        <>
-            <div className="h1">New Room</div>
-            <div className="card">Coming soon.</div>
-        </>
-    );
-}
