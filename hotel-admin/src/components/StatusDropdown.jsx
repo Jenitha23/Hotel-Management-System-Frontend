a34027ee9b315a21e6784f11a7f615a5ddb958b6
J-Jenitha <jenithamaxi17@gmail.com>
2025-10-30 23:16:27 +0530

    Delete hotel-admin directory

--- hotel-admin/src/components/StatusDropdown.jsx
+++
@@ -1,9 +0,0 @@
-const NEXT = ["PENDING","CONFIRMED","CANCELLED","COMPLETED"];
-
-export default function StatusDropdown({ value, onChange }) {
-    return (
-        <select className="select" style={{width:160}} value={value} onChange={(e)=>onChange?.(e.target.value)}>
-            {NEXT.map(s => <option key={s} value={s}>{s}</option>)}
-        </select>
-    );
-}
